45bc1b3bbfb9f28bb95a2333b710841b348299f9
dadat <dvdbarredo@gmail.com>
2016-03-14 15:04:00 +0800

    Updated with hex editor and MD5 string encryption testing.

--- Corgi Basics/Form1.Designer.cs
+++ Corgi Basics/Form1.Designer.cs
@@ -29,22 +29,152 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHex = new System.Windows.Forms.TextBox();
+            this.txtInt = new System.Windows.Forms.TextBox();
+            this.txtConvertMD5 = new System.Windows.Forms.Button();
+            this.txtMD5 = new System.Windows.Forms.TextBox();
+            this.txtIntToMD5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInt);
+            this.groupBox1.Controls.Add(this.txtHex);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 96);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hex:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Int:";
+            // 
+            // txtHex
+            // 
+            this.txtHex.Location = new System.Drawing.Point(171, 50);
+            this.txtHex.Name = "txtHex";
+            this.txtHex.Size = new System.Drawing.Size(153, 20);
+            this.txtHex.TabIndex = 2;
+            // 
+            // txtInt
+            // 
+            this.txtInt.Location = new System.Drawing.Point(171, 27);
+            this.txtInt.Name = "txtInt";
+            this.txtInt.Size = new System.Drawing.Size(153, 20);
+            this.txtInt.TabIndex = 3;
+            // 
+            // txtConvertMD5
+            // 
+            this.txtConvertMD5.Location = new System.Drawing.Point(397, 326);
+            this.txtConvertMD5.Name = "txtConvertMD5";
+            this.txtConvertMD5.Size = new System.Drawing.Size(75, 23);
+            this.txtConvertMD5.TabIndex = 9;
+            this.txtConvertMD5.Text = "Convert";
+            this.txtConvertMD5.UseVisualStyleBackColor = true;
+            this.txtConvertMD5.Click += new System.EventHandler(this.txtConvertMD5_Click);
+            // 
+            // txtMD5
+            // 
+            this.txtMD5.Location = new System.Drawing.Point(171, 53);
+            this.txtMD5.Name = "txtMD5";
+            this.txtMD5.Size = new System.Drawing.Size(153, 20);
+            this.txtMD5.TabIndex = 8;
+            // 
+            // txtIntToMD5
+            // 
+            this.txtIntToMD5.Location = new System.Drawing.Point(171, 27);
+            this.txtIntToMD5.Name = "txtIntToMD5";
+            this.txtIntToMD5.Size = new System.Drawing.Size(153, 20);
+            this.txtIntToMD5.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MD5:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(143, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Int:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtIntToMD5);
+            this.groupBox2.Controls.Add(this.txtMD5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.txtConvertMD5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtInt;
+        private System.Windows.Forms.TextBox txtHex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button txtConvertMD5;
+        private System.Windows.Forms.TextBox txtIntToMD5;
+        private System.Windows.Forms.TextBox txtMD5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- Corgi Basics/Form1.cs
+++ Corgi Basics/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,46 @@ namespace Corgi_Basics
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string CalculateMD5Hash(string input) 
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        string HexFromID(int ID)
+        {
+            return ID.ToString("X");
+        }
+
+        private void txtConvertMD5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtHex.Text = HexFromID(Convert.ToInt16(txtInt.Text));
+
+                txtMD5.Text = CalculateMD5Hash(txtIntToMD5.Text);
+
+                MessageBox.Show(HexFromID(Convert.ToInt16(txtIntToMD5.Text)));
+            }
+            catch (Exception)
+            {
+ 
+            }
+
         }
     }
 }
